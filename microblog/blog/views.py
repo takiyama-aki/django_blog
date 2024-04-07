03da08660a807fa45f48bfeeaa7379cf8a6a5425
akki <takiyamaakihito@gmail.com>
2024-04-07 22:10:58 +0900

    ブログ投稿ボタン(開発中)

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import CommentForm, NewPost
 from .models import Post # データベースをインポート
@@ -6,34 +6,33 @@ from .models import Post # データベースをインポート
 def frontpage(request):
     # Postデータベースのオブジェクトを全て取得する
     posts = Post.objects.all()
-    
+    print("frontpage test")
     # frontpage.htmlをサーバーに返す
     return render(request, "blog/frontpage.html", {"posts": posts}) # "posts"という名前でpostsのデータを渡す
 
 def post_detail(request, slug):
     # Postデータベースのslugが引数のslugのやつと同じオブジェクトを取得する
-    post = Post.objects.get(slug=slug)
+     # Postデータベースのslugが引数のslugのやつと同じオブジェクトを取得する
+    post = get_object_or_404(Post, slug=slug)
 
     if request.method == "POST":
         form = CommentForm(request.POST)
-
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-
-            # どこにページ遷移するのか
-            return redirect("post_detail", slug=post.slug)
-        
+            # コメントが保存されたことをユーザーに通知
+            return render(request, "blog/post_detail.html", {"post": post, "form": form, "comment_saved": True})
     else:
-        form = CommentForm( )
+        form = CommentForm()
 
     return render(request, "blog/post_detail.html", {"post": post, "form": form})
 
 def new_post(request):
     
     # Postデータベースのオブジェクトを全て取得する
-    posts = Post.objects.all()
+    # posts = Post.objects.all()
     
     # frontpage.htmlをサーバーに返す
-    return render(request, "blog/frontpage.html", {"posts": posts}) # "posts"という名前でpostsのデータを渡す
+    print("new post test")
+    return render(request, "blog/new_post.html") # "posts"という名前でpostsのデータを渡す
--- microblog/microblog/urls.py
+++ microblog/microblog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),                       # djangoオリジナルの管理者画面 [user: admin pass: pass1234]
     path("", frontpage),                                   # ""のパスが呼ばれたら, frontpage関数を探す (views.py)
     path("<slug:slug>/", post_detail, name="post_detail"),  # "<slug:~/"のパスが呼ばれたらpost_detail関数を探す, nameはhref属性で使う
-    path("new_post/", frontpage, name="new_post")                            # ("[パス], [メソッド], [hrefのname属性]")
+    path("new_post/", new_post, name="new_post")                            # ("[パス], [メソッド], [hrefのname属性]")
 ]
